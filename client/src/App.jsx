338b056110ec6d9d33bd838c2feedd5a607309d8
RajaSualikheen <rajasualikheen12@gmail.com>
2025-07-21 23:54:04 -0700

    hosting added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/login.jsx";
 import Register from "./pages/register.jsx";
+import Dashboard from "./pages/dashboard.jsx";
 
 function App() {
   return (
   <Home />,
   <Login />,
-  <Register />
+  <Register />,
+  <Dashboard />
   );
 }
 
